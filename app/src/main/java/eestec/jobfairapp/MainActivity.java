ff8bf7714dc541769663171c1b4e6257f50101e7
hanic.dzana <dzana.1h@gmail.com>
2016-09-18 23:09:07 +0200

    added google map with marker in etf sarajevo

--- app/src/main/java/eestec/jobfairapp/MainActivity.java
+++ app/src/main/java/eestec/jobfairapp/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity{
         imgBtn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MapActivity.class));
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
             }
         });
 
