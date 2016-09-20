f65f58d49203bf277c7b1ad50ca7238e3aea7b42
hanic.dzana <dzana.1h@gmail.com>
2016-09-20 21:02:22 +0200

    action bar in map

--- app/src/main/java/eestec/jobfairapp/MapsActivity.java
+++ app/src/main/java/eestec/jobfairapp/MapsActivity.java
@@ -2,6 +2,7 @@ package eestec.jobfairapp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
@@ -25,21 +26,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         LatLng etf = new LatLng(43.856680, 18.398433);
-        mMap.addMarker(new MarkerOptions().position(etf).title("Marker in ETF"));
+        mMap.addMarker(new MarkerOptions().position(etf).title(""));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(etf, 18));
     }
 }
