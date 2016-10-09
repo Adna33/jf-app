950133061c0159b003269137d2812de4bd3e01cc
Adna33 <adna1karkelja@gmail.com>
2016-10-09 12:05:28 +0200

    kompanijeDetaljno

--- app/src/main/java/eestec/jobfairapp/CompanyDetailsActivity.java
+++ app/src/main/java/eestec/jobfairapp/CompanyDetailsActivity.java
@@ -1,8 +1,11 @@
 package eestec.jobfairapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class CompanyDetailsActivity extends AppCompatActivity {
 
@@ -17,6 +20,25 @@ public class CompanyDetailsActivity extends AppCompatActivity {
 
             actionBar.setDisplayHomeAsUpEnabled(true); //Set this to true if selecting "home" returns up by a single level in your UI rather than back to the top level or front page.
             actionBar.setHomeAsUpIndicator(R.drawable.left_arrow); // set a custom icon for the default home button
+        }
+        TextView tv1=(TextView) findViewById(R.id.tv_detalji_company);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("COMPANY_CONTENT");
+            tv1.setText(value);
+
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
