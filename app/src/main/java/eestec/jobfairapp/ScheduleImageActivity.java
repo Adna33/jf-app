6377c28b8072ef0158207bc1810c765643e956cf
Adna33 <adna1karkelja@gmail.com>
2016-10-22 20:41:33 +0200

    Raspored i Skica Activity

--- app/src/main/java/eestec/jobfairapp/ScheduleImageActivity.java
+++ app/src/main/java/eestec/jobfairapp/ScheduleImageActivity.java
@@ -1,7 +1,10 @@
 package eestec.jobfairapp;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ScheduleImageActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class ScheduleImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_image);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null)
+        {
+
+            actionBar.setDisplayHomeAsUpEnabled(true); //Set this to true if selecting "home" returns up by a single level in your UI rather than back to the top level or front page.
+            actionBar.setHomeAsUpIndicator(R.drawable.left_arrow); // set a custom icon for the default home button
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
