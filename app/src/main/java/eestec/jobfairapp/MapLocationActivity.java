49b66ab42ce64d40560598053802bb19d8cdf15a
Adna33 <adna1karkelja@gmail.com>
2016-10-23 17:48:13 +0200

    Mapa

--- app/src/main/java/eestec/jobfairapp/MapLocationActivity.java
+++ app/src/main/java/eestec/jobfairapp/MapLocationActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 /**
  * Created by XMAN on 23.10.2016.
  */
 
 public class MapLocationActivity extends AppCompatActivity {
+    ImageView mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,9 @@ public class MapLocationActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.left_arrow);
         }
+        mapa=(ImageView)findViewById(R.id.location);
+        PhotoViewAttacher photoView = new PhotoViewAttacher(mapa);
+        photoView.update();
     }
 
     @Override
